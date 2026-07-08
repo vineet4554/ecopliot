fef7009c8c23dc42e472122c107dab36996c7806
vineet4554 <vineetchaudhary03102005@gmail.com>
2026-07-08 16:52:23 +0530

    fix(deploy): upgrade Node.js base image to v20 to support mongoose engine requirements

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,10 @@ app.use(securityHeadersMiddleware);
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
+  'http://localhost:3001',
+  'http://127.0.0.1:3001',
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
   'http://localhost:8000',
   'http://127.0.0.1:8000',
   'http://localhost:8001',
